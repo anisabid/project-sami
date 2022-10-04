1de030fecf3af3ecfff0b765025c63aee6d621cc
Anis ABID <anab.tn@gmail.com>
2022-10-04 07:21:23 +0200

    Updateelecscooter

--- elecscooter/js/slider.js
+++ elecscooter/js/slider.js
@@ -1,12 +1,12 @@
 const contSlides = document.querySelectorAll(".js--slider .js--slide").length;
 const $slides = document.querySelector(".js--slides");
-$slides.style.width = contSlides * 100 + "vw";
+$slides.style.width = contSlides * 99 + "vw";
 var currentSlide = 0;
 
 function changeSlide() {
   currentSlide++;
   currentSlide = currentSlide % contSlides === 0 ? 0 : currentSlide;
-  $slides.style.marginLeft = -(currentSlide * 100) + "vw";
+  $slides.style.marginLeft = -(currentSlide * 99) + "vw";
 }
 
-setInterval("changeSlide()", 2000);
+setInterval("changeSlide()", 4000);
